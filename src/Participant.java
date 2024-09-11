6b6f5ffaaaf82084fe7ea6ea8fe694ecb76b2493
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-11 19:41:08 +0200

    Define participant class

---
+++ src/Participant.java
@@ -0,0 +1,18 @@
+/**
+ * Participant of the competition.
+ */
+class Participant {
+    String name;
+    int score;
+
+    /**
+     * Initializes an instance of this class.
+     *
+     * @param name - Name of participant.
+     * @param score - Participant's score.
+     */
+    Participant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
